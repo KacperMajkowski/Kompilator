cdb5ee62ce05424bdd2c0de796944e630dbde233
KacperMajkowski <kacper20015@o2.pl>
2022-12-16 20:16:47 +0100

    Added VM

--- Lexer.py
+++ Lexer.py
@@ -2,15 +2,28 @@ from sly import Lexer
 
 
 class CompLexer(Lexer):
-    tokens = {"A", "B", "C"}
+    tokens = {"identifier", "num", "READ", "WRITE", "PROGRAM_IS", "VAR", "BEGIN", "END"}
     
-    A = r'ab'
-    B = r'cba'
-    C = r'c'
+    ignore = ' \t'
+    ignore_semicolon = ';'
+    ignore_comma = ','
+    ignore_comment = r'\[.*\]'
+    ignore_newline = r'\n+'
+    
+    num = r'\d+'
+    READ = r'READ'
+    WRITE = r'WRITE'
+    PROGRAM_IS = r'PROGRAM IS'
+    VAR = r'VAR'
+    BEGIN = r'BEGIN'
+    END = r'END'
+    
+    identifier = r'[a-zA-Z_][a-zA-Z0-9_]*'
     
     
 if __name__ == '__main__':
-    data = 'abcbac'
+    data = open("program.txt").read()
     lexer = CompLexer()
     for tok in lexer.tokenize(data):
         print('type=%r, value=%r' % (tok.type, tok.value))
+        
---
+++ Parser.py
@@ -0,0 +1,85 @@
+from sly import Parser
+from Lexer import CompLexer
+
+
+class CompParser(Parser):
+    
+    tokens = CompLexer.tokens
+    nextFreeIndex = 1
+    currContext = 0     #0 - main, następne dla kolejnych funkcji
+    
+    contexts = ["main"]
+    variables = [[0, "acc"]]
+    
+    out = ""
+
+    @_("main")
+    def program_all(self, p):
+        pass
+    
+    @_("PROGRAM_IS VAR declarations BEGIN commands END")
+    def main(self, p):
+        pass
+    
+    @_("declarations identifier")
+    def declarations(self, p):
+        print("Declare variable", p[1])
+        self.variables.append([self.currContext, p[1]])
+        
+    @_("identifier")
+    def declarations(self, p):
+        print("Declare variable", p[0])
+        self.variables.append([self.currContext, p[0]])
+        
+    @_("commands command")
+    def commands(self, p):
+        pass
+
+    @_("command")
+    def commands(self, p):
+        pass
+        
+    @_("READ identifier")
+    def command(self, p):
+        print("Read variable", p[1], "to cell", self.nextFreeIndex)
+        self.nextFreeIndex += 1
+
+    @_("WRITE value")
+    def command(self, p):
+        print("Put variable with index", p[1])
+        self.out += "PUT" + str(p[1]) + "\n"
+        
+    @_("identifier")
+    def value(self, p):
+        return getVarCellIndex(self.variables, self.currContext, p[0])
+
+    @_("num")
+    def value(self, p):
+        self.out += "SET" + str(p[0]) + "\n"
+        return 0
+
+    def error(self, t):
+        print("Illegal character '%s'" % t)
+
+
+def getFuncContext(f, functions):
+    return functions.index(f)
+
+
+def getVarCellIndex(variables, currContext, x):
+    for cellIndex in range(len(variables)):
+        if currContext == variables[cellIndex][0] and x == variables[cellIndex][1]:
+            return cellIndex
+    print(variables, currContext, x, "not found")
+  
+  
+if __name__ == '__main__':
+    lexer = CompLexer()
+    parser = CompParser()
+
+    text = open("program.txt").read()
+    result = parser.parse(lexer.tokenize(text))
+    print(" ")
+    print(parser.out)
+    #print(parser.variables)
+    
